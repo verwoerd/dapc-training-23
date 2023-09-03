adb7e3288f16bcb64ba3da2e9517b602bf23507b
Maarten Sijm <9739541+mpsijm@users.noreply.github.com>
2023-09-03 14:14:06 +0200

    session-2.tex: Feedback

--- code/session-1/python/bapc-f.py
+++ code/session-1/python/bapc-f.py
@@ -5,12 +5,11 @@ def calculate(wind):
   high = low + 90
   curr = low + 45
   for c in wind[-3::-1]:
-    if end.index(c) == 0:
-      high = curr
-    else:
-      low = curr
+    if end.index(c) == 0: high = curr
+    else: low = curr
     curr = (low + high) / 2
   return curr
 
-X, Y = sorted("NESW".index(d[0]) * 90 if len(d) == 1 else calculate(d) for d in input().split())
+X, Y = sorted("NESW".index(d[0]) * 90 if len(d) == 1 else calculate(d)
+              for d in input().split())
 print(min(Y - X, 360 + X - Y))
